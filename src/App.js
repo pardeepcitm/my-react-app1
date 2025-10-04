824a0cb8225ae4080ecde8225576e6a4b2a432f3
pardeepcitm <63185204+pardeepcitm@users.noreply.github.com>
2025-10-04 22:10:47 +0200

    added talwind

--- src/App.js
+++ src/App.js
@@ -19,25 +19,16 @@ const offers = [
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      <img src="/scandiwhispers.png" alt="Site Logo" className="site-logo" />
-        <h1>Latest Shop Offers</h1>
-        <div className="offers-list">
-          {offers.map((offer, idx) => (
-            <div key={idx} className="offer-card">
-              <h2>{offer.shop}</h2>
-              <h3>{offer.title}</h3>
-              <p>{offer.details}</p>
-              <a href={offer.url} target="_blank" rel="noopener noreferrer">
-                Visit Shop
-              </a>
-            </div>
-          ))}
-        </div>
-      </header>
+    <div>
+      <h1 className="text-3xl font-bold underline text-center">
+      Hello world!
+    </h1>
+      <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-center' >My button</button>
     </div>
+    
+    
   );
 }
+
 
 export default App;
