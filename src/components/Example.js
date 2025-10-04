3f20a65bc2dc5c3f5c97f685b7c4cfbae45975aa
pardeepcitm <63185204+pardeepcitm@users.noreply.github.com>
2025-10-04 22:50:50 +0200

    ffixed color for mobile

--- src/components/Example.js
+++ src/components/Example.js
@@ -15,7 +15,7 @@ export default function Example() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
   return (
-    <div className="bg-white dark:bg-pink-300">
+    <div className="dark:bg-pink-300">
       <header className="absolute inset-x-0 top-0 z-50">
         <nav aria-label="Global" className="flex items-center justify-between p-6 lg:px-8">
           <div className="flex lg:flex-1">
