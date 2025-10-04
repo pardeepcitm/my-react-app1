be33b34ed923a61f42b922f9a8e698e11a1badaa
pardeepcitm <63185204+pardeepcitm@users.noreply.github.com>
2025-10-04 22:45:29 +0200

    changed color

--- src/components/Example.js
+++ src/components/Example.js
@@ -15,7 +15,7 @@ export default function Example() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
   return (
-    <div className="bg-white dark:bg-gray-900">
+    <div className="bg-white dark:bg-pink-300">
       <header className="absolute inset-x-0 top-0 z-50">
         <nav aria-label="Global" className="flex items-center justify-between p-6 lg:px-8">
           <div className="flex lg:flex-1">
@@ -124,7 +124,7 @@ export default function Example() {
         </div>
         <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
           <div className="hidden sm:mb-8 sm:flex sm:justify-center">
-            <div className="relative rounded-full px-3 py-1 text-sm/6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20 dark:text-gray-400 dark:ring-white/10 dark:hover:ring-white/20">
+            <div className="relative rounded-full px-3 py-1 text-sm/6 text-white ring-1 ring-gray-900/10 hover:ring-gray-900/20 dark:text-white dark:ring-white/10 dark:hover:ring-white/20">
               ScandiWhispers is coming soom..{' '}
               <a href="#" className="font-semibold text-indigo-600 dark:text-indigo-400">
                 <span aria-hidden="true" className="absolute inset-0" />
@@ -136,7 +136,7 @@ export default function Example() {
             <h1 className="text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl dark:text-white">
               Get to know all the offers/deals here
             </h1>
-            <p className="mt-8 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8 dark:text-gray-400">
+            <p className="mt-8 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8 dark:text-white">
               wait for some more days.
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
